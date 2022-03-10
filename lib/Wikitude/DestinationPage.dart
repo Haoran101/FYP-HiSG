8916dac1a3b99ab18f32f390147c6711e7937bed
haoran <whr991001@gmail.com>
2022-03-11 04:25:56 +0800

    init map direct page

--- lib/Wikitude/DestinationPage.dart
+++ lib/Wikitude/DestinationPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:wikitude_flutter_app/DataSource/location_provider.dart';
 import 'package:wikitude_flutter_app/Wikitude/arview.dart';
 import 'package:wikitude_flutter_app/Wikitude/sample.dart';
 
@@ -12,14 +14,42 @@ class DestinationPage extends StatefulWidget {
 
 class _DestinationPageState extends State<DestinationPage> {
 
+  late GoogleMapController mapController;
+
+  late LatLng _center;
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  void initState() {
+    determinePosition().then((pos) => 
+    _center = LatLng(pos.latitude, pos.longitude));
+    super.initState();
+  }
+
   switchToARView() {
     //TODO: push view to request for destination
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ArViewWidget(sample: this.widget.sample)));
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Search For Destination'),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        body: Container(
+          height: 900,
+          child: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+          ),
+        ),
+      );
   }
 }
